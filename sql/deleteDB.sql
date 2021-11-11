19949b5a7548917225c8a2a9daaba25ca7ec2642
tsobczak11 <86012220+tsobczak11@users.noreply.github.com>
2021-11-10 18:26:10 -0600

    Add files via upload

---
+++ sql/deleteDB.sql
@@ -0,0 +1,9 @@
+DROP TABLE farm;
+DROP TABLE transport;
+DROP TABLE orders;
+DROP TABLE recipient;
+DROP TABLE available_surplus;
+DROP TABLE crop;
+DROP TABLE category;
+DROP TABLE location;
+DROP TABLE quantity;
